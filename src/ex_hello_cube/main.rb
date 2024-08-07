42e8b9ec6b600102558712a88033ec15778ee1af
Gadgeteer70 <jgrundman@gmail.com>
2024-08-07 15:25:03 -0400

    commit

--- src/ex_hello_cube/main.rb
+++ src/ex_hello_cube/main.rb
@@ -8,8 +8,11 @@ module Examples
 
     def self.create_cube
       model = Sketchup.active_model
+      puts "var model set"
       model.start_operation('Create Cube', true)
       group = model.active_entities.add_group
+
+      puts "var group set"
       entities = group.entities
       points = [
         Geom::Point3d.new(0,   0,   0),
@@ -17,6 +20,9 @@ module Examples
         Geom::Point3d.new(1.m, 1.m, 0),
         Geom::Point3d.new(0,   1.m, 0)
       ]
+      points.each do { |pnt|
+        puts "index " + pnt.index + " = " pnt.to_s
+      }
       face = entities.add_face(points)
       face.pushpull(-1.m)
       model.commit_operation
